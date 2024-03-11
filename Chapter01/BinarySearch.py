88c23fc4a2e9629e2edb52261425e3ecfbc747a8
Guilherme China <echina725@gmail.com>
2024-03-11 07:41:33 -0300

    add binary search algorithm

---
+++ Chapter01/BinarySearch.py
@@ -0,0 +1,35 @@
+def binary_search(ordered_list:list[int], target_item:int):
+    """
+    if 'target_item' exists in 'ordered_list', then return its location index,
+    if not, returns None.
+
+    Arguments:
+        ordered_list: [int] -> where to look after 'target_item'
+        target_item: int
+
+    Return: int | None
+    """
+
+    lower_index = 0
+    greater_index = len(ordered_list) - 1
+
+    while lower_index <= greater_index:
+        middle_index = (lower_index + greater_index) // 2
+
+        middle_item = ordered_list[middle_index]
+
+        if(middle_item==target_item):
+            return middle_index
+
+        if(middle_item > target_item):
+            greater_index = middle_index - 1
+        
+        else:
+            lower_index = middle_index + 1
+    
+    return None
+
+if __name__ == "__main__":
+    ORDERED_LIST = [1, 3, 5, 7, 9, 11]
+    print(binary_search(ORDERED_LIST, target_item=7))
+    print(binary_search(ORDERED_LIST, target_item=10))
